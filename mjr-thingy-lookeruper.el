835cdc83f215fbc96f47df12c78275af3dcc1fba
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-09-02 11:00:37 -0500

    Added git-commit-ediff.

--- mjr-thingy-lookeruper.el
+++ mjr-thingy-lookeruper.el
@@ -19,7 +19,7 @@
 ;; TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;; Author:      Mitch Richling
-;; Version:     1.36
+;; Version:     1.37
 ;; Keywords:    mjr-thingy-lookeruper
 ;; URL:         https://github.com/richmit/mjr-thingy-lookeruper
 
@@ -205,6 +205,25 @@
           :desc "Lookup an ISBN (10 or 13) number on isbnsearch.org using browse-url (emacs)"
           :atpt (lambda () (and (thing-at-point-looking-at "\\b\\([0-9]\\{10\\}\\|[0-9]-[0-9]\\{6\\}-[0-9][0-9]-[0-9]\\|[0-9]\\{13\\}\\|[0-9]\\{3\\}-[0-9]-[0-9]\\{5\\}-[0-9]\\{3\\}-[0-9]\\)\\b" 25) (match-string 1)))
           :actn (lambda (thingy) (browse-url (concat "https://isbnsearch.org/isbn/" (url-hexify-string (replace-regexp-in-string "[^0-9]" "" thingy))))))
+    (when (executable-find "git")
+      (list :name "git-commit"
+            :desc "Lookup a git commit via a full 40 character hexadecimal commit ID."
+            :reqp (lambda () (file-directory-p ".git"))
+            :atpt (lambda () (and (thing-at-point-looking-at "\\b\\([0-9a-f]\\{40\\}\\)\\b" 41) (match-string 1)))
+            :tokp (lambda (q) (and (stringp q) (string-match-p "\\`[0-9a-f]\\{40\\}\\'" q)))
+            :actn "git show %Q"))
+    (when (executable-find "git")
+      (list :name "git-commit-ediff"
+            :desc "Ediff a file that changed between a git commit (identified by a full 40 character hexadecimal commit ID) and working."
+            :reqp (lambda () (file-directory-p ".git"))
+            :atpt (lambda () (and (thing-at-point-looking-at "\\b\\([0-9a-f]\\{40\\}\\)\\b" 41) (match-string 1)))
+            :tokp (lambda (q) (and (stringp q) (string-match-p "\\`[0-9a-f]\\{40\\}\\'" q)))
+            :actn (lambda (thingy)
+                    (let ((changed-files (process-lines "git" "diff" "--name-only" thingy)))
+                      (unless changed-files (error "Could not construct changed file list!"))
+                      (let ((file-to-diff (ido-completing-read "File to diff: " changed-files nil t)))
+                        (unless file-to-diff (error "Something went wrong reading file!"))
+                        (vc-version-ediff (list file-to-diff) thingy nil))))))
    ;; Common stuff
     (list :name "man"
           :desc "Look for a UNIX man page via man (emacs)."
@@ -289,13 +308,6 @@
                                  (string-match "\\`\\(\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)\\)\\'" q)
                                  (cl-every (lambda (x) (let ((y (string-to-number (match-string x q)))) (and (<= 0 y) (>= 255 y)))) '(2 3 4 5))))
           :actn #'dns-lookup-host)
-    (when (executable-find "git")
-      (list :name "git-commit"
-            :desc "Lookup a commit in windows user name (PowerShell)."
-            :reqp (lambda () (file-directory-p ".git"))
-            :atpt (lambda () (and (thing-at-point-looking-at "\\b\\([0-9a-f]\\{40\\}\\)\\b" 41) (match-string 1)))
-            :tokp (lambda (q) (and (stringp q) (string-match-p "\\`[0-9a-f]\\{40\\}\\'" q)))
-            :actn "git show %Q"))
     )))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
